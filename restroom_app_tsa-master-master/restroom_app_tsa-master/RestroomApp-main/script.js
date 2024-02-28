7bca258d432238d68fd6de0ddb07b3c1fbb1591c
Firolo8 <145720539+Firolo8@users.noreply.github.com>
2024-02-28 15:04:16 -0600

    UI Updates

--- restroom_app_tsa-master-master/restroom_app_tsa-master/RestroomApp-main/script.js
+++ restroom_app_tsa-master-master/restroom_app_tsa-master/RestroomApp-main/script.js
@@ -1,3 +1,13 @@
 function menuAnimation(x) {
     x.classList.toggle("change");
 }
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
